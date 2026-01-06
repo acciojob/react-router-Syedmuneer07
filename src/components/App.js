c1aa7c7c31bc1f5606b9d46b63f03f4b27eeb0f0
Syed muneerul zakireen <syedmuneerzakk@gmail.com>
2026-01-06 18:59:52 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
-
 import React from "react";
 import './../styles/App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
 
 const App = () => {
   return (

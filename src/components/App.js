9ead6f5ed8c3d23d62bc534b5f4a0be6f6d8889b
Syed muneerul zakireen <syedmuneerzakk@gmail.com>
2026-01-06 18:50:17 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,25 @@ import './../styles/App.css';
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
+  
+    <Router>
+      <nav>
+        <ul>
+          <li>
+              <Link to="/">Home</Link>
+          </li>
+          <li>
+              <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+    <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/about" element={<About/>}></Route>
+    </Routes>
+  
+    </Router>
+    
   )
 }
 
